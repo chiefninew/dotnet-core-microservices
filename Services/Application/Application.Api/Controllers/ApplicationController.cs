7e721dcbd804aca2c0446cb374ce8ba38d6c9f64
chiefninew <oninpaul15@gmail.com>
2022-08-03 18:04:21 +0800

    init

---
+++ Services/Application/Application.Api/Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly ILogger<ApplicationController> _logger;
+
+        public ApplicationController(ILogger<ApplicationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetApplicationController")]
+        public string Get()
+        {
+            return "This is Application Microservice";
+        }
+    }
+}
---
+++ Services/Identity/Identity.Api/Controllers/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IdentityController : ControllerBase
+    {
+        private readonly ILogger<IdentityController> _logger;
+
+        public IdentityController(ILogger<IdentityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetIdentityController")]
+        public string Get()
+        {
+            return "This is Identity Microservice";
+        }
+    }
+}
